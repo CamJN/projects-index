b4e872948e19b693d2f00d002a80e95250edc485
Camden Narzt <admin@silversshadows.com>
2014-11-08 13:19:17 -0700

    extract config

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+defined( 'runtime' ) or die;
+
+$hosts_file= "/etc/hosts";
+$dev_tld = '.dev';
+$sites_dir = '/Users/camdennarzt/Sites/';
+$title = 'CJN';
+$name = 'Camden Narzt';
+
+$prod_domains = array(
+  array('title' => "Metabolistics Inc.",
+        'link'=>"https://www.metabolistics.com",
+        'img'=>"https://metabolistics.dev/metabolistics.dev.png"
+  ),
+  array('title' => "King Of Strain",
+        'link'=>"http://kingofstrain.com",
+        'img'=>"http://kingofstrain.dev/kingofstrain.dev.png"
+  ),
+  array('title' => "MoteScan for WiTAP™",
+        'link'=>"http://mote.scanimetrics.com",
+        'img'=>"http://mote.scanimetrics.dev/mote.scanimetrics.dev.png"
+  )
+);
+
+$contacts = array(
+  array('title'=>"Twitter",'link'=>"https://twitter.com/CamJN"),
+  array('title'=>"Email",'link'=>"mailto:admin@silversshadows.com"),
+  array('title'=>"GitHub",'link'=>"https://github.com/CamJN/")
+);
