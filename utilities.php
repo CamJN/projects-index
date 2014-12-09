ba00725c4103029e18d1336a718340597117c73b
Camden Narzt <admin@silversshadows.com>
2014-12-09 13:19:48 -0700

    allow multiple lines in hosts file

--- utilities.php
+++ utilities.php
@@ -20,15 +20,17 @@ function gh_url($contacts){
 }
 
 function domains($hosts_file,$dev_tld){
-  if (file_exists($hosts_file)) {
-    $filtered = array_filter(file($hosts_file),function($e)use($dev_tld){ return strpos($e,$dev_tld) !== FALSE; });
-    $domains = explode(' ',array_pop($filtered));
-    array_walk($domains,function(&$d){$d = htmlspecialchars(trim($d));});
-    $domains = array_filter($domains,function($e)use($dev_tld){ return strpos($e,$dev_tld) !== FALSE; });
-  }else{
-    $domains = [];
-  }
-  return $domains;
+    if (file_exists($hosts_file)) {
+        $filtered = array_filter(file($hosts_file),function($e){ return strpos($e,'::1') !== FALSE; });
+        $mDomains = array_map(function($line){return explode(' ',$line);},$filtered);
+        $aDomains = array_reduce($mDomains,'array_merge',array());
+        $domains = array_filter($aDomains,function($e)use($dev_tld){ return strpos($e,$dev_tld) !== FALSE; });
+        array_walk($domains,function(&$d){$d = htmlspecialchars(trim($d));});
+        $domains = array_filter($domains,function($e)use($dev_tld){ return strpos($e,$dev_tld) !== FALSE; });
+    }else{
+        $domains = [];
+    }
+    return $domains;
 }
 
 ?>
