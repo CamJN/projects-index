b3b0ed0fc6807ad2d148f10b5d95dd26b35a420c
Camden Narzt <admin@silversshadows.com>
2015-01-25 23:15:42 -0700

    add arewesmallyet live page

--- config.php
+++ config.php
@@ -8,30 +8,34 @@ $title = 'CJN';
 $name = 'Camden Narzt';
 
 $prod_domains = array(
-  array('title' => "Metabolistics Inc.",
-        'link'=>"https://www.metabolistics.com",
-        'img'=>"https://metabolistics.dev/metabolistics.dev.png"
-  ),
-  array('title' => "King Of Strain",
-        'link'=>"http://kingofstrain.com",
-        'img'=>"http://kingofstrain.dev/kingofstrain.dev.png"
-  ),
-  array('title' => "MoteScan for WiTAP™",
-        'link'=>"http://mote.scanimetrics.com",
-        'img'=>"http://mote.scanimetrics.dev/mote.scanimetrics.dev.png"
-  ),
-  array('title' => "Ares Edmonton Membership",
-        'link'=>"http://membership.aresedmonton.com",
-        'img'=>"http://membership.aresedmonton.dev/membership.aresedmonton.dev.png"
-  ),
-  array('title' => "Silver's Shadows Splash Page",
-        'link'=>"http://splash.silversshadows.com",
-        'img'=>"http://splash.silversshadows.dev/splash.silversshadows.dev.png"
-  )
+    array('title' => "Metabolistics Inc.",
+          'link'=>"https://www.metabolistics.com",
+          'img'=>"https://metabolistics.dev/metabolistics.dev.png"
+    ),
+    array('title' => "King Of Strain",
+          'link'=>"http://kingofstrain.com",
+          'img'=>"http://kingofstrain.dev/kingofstrain.dev.png"
+    ),
+    array('title' => "MoteScan for WiTAP™",
+          'link'=>"http://mote.scanimetrics.com",
+          'img'=>"http://mote.scanimetrics.dev/mote.scanimetrics.dev.png"
+    ),
+    array('title' => "Ares Edmonton Membership",
+          'link'=>"http://membership.aresedmonton.com",
+          'img'=>"http://membership.aresedmonton.dev/membership.aresedmonton.dev.png"
+    ),
+    array('title' => "Silver's Shadows Splash Page",
+          'link'=>"http://splash.silversshadows.com",
+          'img'=>"http://splash.silversshadows.dev/splash.silversshadows.dev.png"
+    ),
+    array('title' => "Are We Small Yet",
+          'link'=>"http://camjn.github.io/arewesmallyet",
+          'img'=>"http://arewesmallyet.dev/arewesmallyet.dev.png"
+    )
 );
 
 $contacts = array(
-  array('title'=>"Twitter",'link'=>"https://twitter.com/CamJN"),
-  array('title'=>"Email",'link'=>"mailto:admin@silversshadows.com"),
-  array('title'=>"GitHub",'link'=>"https://github.com/CamJN/")
+    array('title'=>"Twitter",'link'=>"https://twitter.com/CamJN"),
+    array('title'=>"Email",'link'=>"mailto:admin@silversshadows.com"),
+    array('title'=>"GitHub",'link'=>"https://github.com/CamJN/")
 );
